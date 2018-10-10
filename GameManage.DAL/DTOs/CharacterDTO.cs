66620fef85ea620504647a7bb9030632a96d9834
DSperber <dennissperber@live.nl>
2018-10-11 00:18:24 +0200

    Added DAL start

---
+++ GameManage.DAL/DTOs/CharacterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManage.DAL.DTOs
+{
+    public struct CharacterDTO
+    {
+        public readonly int Character_Id;
+        public readonly string Name;
+        public readonly int Score;
+
+        public CharacterDTO(int character_Id, string name, int score)
+        {
+            Character_Id = character_Id;
+            Name = name;
+            Score = score;
+        }
+    }
+}
